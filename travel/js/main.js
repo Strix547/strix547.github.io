a2a393c7de3430a1dedab53f9a697fbfca480898
strix547 <strix547@mail.ru>
2017-11-01 23:57:41 +0300

    Initial commit

--- travel/js/main.js
+++ travel/js/main.js
@@ -12,11 +12,44 @@ var swiper = new Swiper('.swiper-container' ,{
 	lazyLoadingInPrevNext: true,
 	nextButton: '.swiper-button-next',
 	   prevButton: '.swiper-button-prev',
-	coverflow: {
+		coverflow: {
 		rotate: 0,
 		stretch: 0,
 		depth: 250,
 		modifier: 1,
 		slideShadows : false,
 	}
+});
+$('.slick-slider').slick({
+	speed: 300,
+	responsive: [
+	  {
+	    breakpoint: 1000,
+	    settings: {
+	      slidesToShow: 2,
+	      slidesToScroll: 1,
+	      infinite: true,
+	      dots: false
+	    }
+	  },
+	  {
+	    breakpoint: 600,
+	    settings: {
+	      slidesToShow: 1,
+	      slidesToScroll: 1,
+	      infinite: true,
+	      dots: false
+	    }
+	  },
+	  {
+	    breakpoint: 480,
+	    settings: {
+	      slidesToShow: 1,
+	      slidesToScroll: 1
+	    }
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
 });
