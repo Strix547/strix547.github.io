78dfeb918ab3a592ff8030e657fba0f4937dbbc7
strix547 <strix547@mail.ru>
2017-12-06 16:06:00 +0300

    Initial commit

---
+++ AutoSchool/js/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("a.group").fancybox({
+        'transitionIn'	:	'elastic',
+        'transitionOut'	:	'elastic',
+        'overlayShow' : true,
+        'overlayOpacity' : 0.5,
+        'speedIn'		:	600,
+        'speedOut'		:	200
+    });
+});
