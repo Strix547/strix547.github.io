2be97d37b0a171f300f50eeefc755349181eeb81
Strix <strix547@mail.ru>
2018-02-16 15:34:18 +0200

    Initial commit

--- elizaveta/js/main.js
+++ elizaveta/js/main.js
@@ -1,23 +1,49 @@
 $(document).ready(function() {
-    $('#pull').click( function(event){
-        event.preventDefault();
-        var menu = document.querySelector("header .top-line nav ul");
-        if (menu.style.display === "block") {
-            $('header .top-line ul')
-                .css('display', 'none')
-                .animate({display: 'none', opacity: 0, top: '-100px'}, 400);
-        } else {
-            $('header .top-line ul')
-                .css('display', 'block')
-                .animate({display: 'block', opacity: 1, top: '0'}, 400);
-        }
-
-    });
-
     $("header .top-line").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 750);
     });
+
+    if($(window).width() > 1280) {
+        var menu = document.querySelector('#menu');
+        menu.style.position = 'static';
+        menu.style.visibility = 'visible';
+        menu.style.height = 'auto';
+    }
+
+    $("#phone").mask("+7 (999) 999 - 99 - 99");
+});
+
+(function() {
+
+    "use strict";
+
+    var toggles = document.querySelectorAll(".c-hamburger");
+
+    for (var i = toggles.length - 1; i >= 0; i--) {
+        var toggle = toggles[i];
+        toggleHandler(toggle);
+    };
+
+    function toggleHandler(toggle) {
+        toggle.addEventListener( "click", function(e) {
+            e.preventDefault();
+            (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+        });
+    }
+
+})();
+
+$("#pull").click(function() {
+    $("#menu").toggleClass("open-menu");
+});
+
+$(document).mouseup(function(e) {
+    var $target = $(e.target);
+    if ($target.closest("#menu").length == 0) {
+        $("#menu").removeClass("open-menu");
+        $("#pull").removeClass("is-active");
+    }
 });
