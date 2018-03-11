e7292423d57c5154f290cb02fee1b7f79e4401d0
Strix <strix547@mail.ru>
2018-03-12 00:44:24 +0200

    Initial commit

--- unionkaper/js/pages/description.js
+++ unionkaper/js/pages/description.js
@@ -2,4 +2,27 @@ $(function() {
     var elem = document.querySelector('.js-switch');
     var init = new Switchery(elem);
     $(".pick_date").AnyTime_picker({ format: "%M" });
+
+    var grayBlock = document.querySelector(".under-header.gray-block");
+    var closePopup = document.querySelector(".close");
+
+    document.body.addEventListener("click", function (e) {
+        var modals = document.querySelectorAll(".modal");
+        if (e.target.classList.contains("close")) {
+            for (var i = 0; i < modals.length; i++) {
+                modals[i].style.display = "none";
+            }
+        }
+    });
+
+    grayBlock.addEventListener("click", function(e) {
+        var modalBuy = document.querySelector("#modal-buy");
+        var modalSell = document.querySelector("#modal-sell");
+        if (e.target.classList.contains("buy")) {
+            modalBuy.style.display = "block";
+        }
+        if (e.target.classList.contains("sell")) {
+            modalSell.style.display = "block";
+        }
+    });
 });
